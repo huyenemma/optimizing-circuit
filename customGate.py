7fdac2786b59ca4775f3cc55ebd7bb5fe93b12ca
huyenemma <huyendo.emm@gmail.com>
2023-05-31 14:29:33 +0300

    add class custom gate

---
+++ customGate.py
@@ -0,0 +1,24 @@
+import cirq
+import numpy as np
+
+class control_not_not(cirq.Gate):
+    def __init__(self):
+        super(CXX, self)
+
+    def _num_qubits_(self):
+        return 3
+
+    def _unitary_(self):
+        return np.array([
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        ])
+
+    def _circuit_diagram_info_(self, args):
+        return "@", "X", "X"
