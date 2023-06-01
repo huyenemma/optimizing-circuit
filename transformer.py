f021f80655f3a7ebe75a64143d477a03b8f9c2c5
huyenemma <huyendo.emm@gmail.com>
2023-06-01 19:07:39 +0300

    edit 2 function

--- transformer.py
+++ transformer.py
@@ -1,4 +1,5 @@
 from cirq import *
+from customGate import CXXX, CXX
 
 
 def cancel_adj_h(circuit):
@@ -63,10 +64,8 @@ def cancel_adj_cnot(circuit):
                 qubit = op.qubits[0]
                 is_qb_in_cnot_gates = any(qubit in key for key in cnot_gates.keys())
                 # if current operation is not CNOT, the previous CNOT (if existed) should be added
-                if isinstance(prev_op.gate, CXPowGate) and prev_op is not None and is_qb_in_cnot_gates:
-                    control = prev_op.qubits[0]
-                    target = prev_op.qubits[1]
-                    opt_circuit.append(prev_op)
+                if is_qb_in_cnot_gates:
+                    opt_circuit.append(cnot_gates[(control, target)])
                     del cnot_gates[(control, target)]
 
                 # add the current operation
@@ -81,37 +80,95 @@ def cancel_adj_cnot(circuit):
     return opt_circuit
 
 
-def two_cx_to_toffoli(circuit):
+def cancel_adj_cnot(circuit):
+    """
+    Apply the template c: a sequence of two CNOT gates is cancelled
+    :param circuit: a circuit to optimize
+    :return: new circuit
+    """
     opt_circuit = Circuit()
-    # dictionary to keep track CNOT gates on control qubits in circuit
+    # dictionary to keep track CNOT gates on tuple (control, target) qubits in circuit
     cnot_gates = {}
-    prev_op = None
     for moment in circuit:
         for op in moment:
             if isinstance(op.gate, CXPowGate):
                 control = op.qubits[0]
                 target = op.qubits[1]
-                if control in cnot_gates.keys():
-                    opt_circuit.append(TOFFOLI(control, cnot_gates[control].qubits[1], target))
-                    del cnot_gates[control]
+                if (control, target) in cnot_gates.keys():
+                    del cnot_gates[(control, target)]
                     continue  # Skip adding the current CNOT gate
                 else:
                     # store the current CNOT gate into dictionary
-                    cnot_gates[control] = op
+                    cnot_gates[(control, target)] = op
+
+                    pre_control, pre_target = list(cnot_gates.keys())[0]
+                    if pre_control != control or pre_target != target:
+                        opt_circuit.append(cnot_gates[(pre_control, pre_target)])
+                        del cnot_gates[(pre_control, pre_target)]
             else:
                 qubit = op.qubits[0]
-                is_qb_in_cnot_gates = any(qubit in key for key in cnot_gates.keys())
+                is_qb_in_cnot_gates = False
+                for key in cnot_gates.keys():
+                    control, target = key
+                    if qubit == control or qubit == target:
+                        is_qb_in_cnot_gates = True
+                        break
                 # if current operation is not CNOT, the previous CNOT (if existed) should be added
-                if isinstance(prev_op.gate, CXPowGate) and prev_op is not None and is_qb_in_cnot_gates:
-                    control = prev_op.qubits[0]
-                    target = prev_op.qubits[1]
-                    opt_circuit.append(prev_op)
+                if is_qb_in_cnot_gates:
+                    opt_circuit.append(cnot_gates[(control, target)])
                     del cnot_gates[(control, target)]
 
                 # add the current operation
                 opt_circuit.append(op)
-            prev_op = op
 
+    # add any remaining CNOT gates in the dictionary to the optimized circuit
+    for operation in cnot_gates.values():
+        if operation is not None:
+            opt_circuit.append(operation)
+
+    return opt_circuit
+
+
+def two_cx_to_cxx(circuit):
+    opt_circuit = Circuit()
+    # dictionary to keep track CNOT gates on control qubits in circuit
+    cnot_gates = {}
+    for moment in circuit:
+        for op in moment:
+            if isinstance(op.gate, CXPowGate):
+                control = op.qubits[0]
+                target = op.qubits[1]
+                if not cnot_gates:
+                    cnot_gates[(control, target)] = op
+                else:
+                    pre_control, pre_target = list(cnot_gates.keys())[0]
+
+                    if pre_control == control and pre_target != target:
+                        opt_circuit.append(CXX().on(control, pre_target, target))
+                        del cnot_gates[(control, pre_target)]
+                        continue  # Skip adding the current CNOT gate
+                    else:
+                        # store the current CNOT gate into dictionary
+                        cnot_gates[(control, target)] = op
+                        if pre_control != control or pre_target == target:
+                            opt_circuit.append(cnot_gates[(pre_control, pre_target)])
+                            opt_circuit.append(cnot_gates[(control, target)])
+                            del cnot_gates[(pre_control, pre_target)]
+            else:
+                qubit = op.qubits[0]
+                is_qb_in_cnot_gates = False
+                for key in cnot_gates.keys():
+                    control, target = key
+                    if qubit == control or qubit == target:
+                        is_qb_in_cnot_gates = True
+                        break
+                # if current operation is not CNOT, the previous CNOT (if existed) should be added
+                if is_qb_in_cnot_gates:
+                    opt_circuit.append(cnot_gates[(control, target)])
+                    del cnot_gates[(control, target)]
+
+                # add the current operation
+                opt_circuit.append(op)
     # add any remaining CNOT gates in the dictionary to the optimized circuit
     for operation in cnot_gates.values():
         if operation is not None:
